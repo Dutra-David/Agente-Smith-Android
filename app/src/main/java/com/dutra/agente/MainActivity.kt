192716caf6f9483752051dcefe9bdb100cfcdd24
Dutra-David <david.kuerten@gmail.com>
2025-12-18 20:36:38 -0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/dutra/agente/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.dutra.agente
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.dutra.agente.ui.screens.ChatScreen
+import com.dutra.agente.ui.theme.AgenteSmiththeme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AgenteSmiththeme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ChatScreen()
+                }
+            }
+        }
+    }
+}
