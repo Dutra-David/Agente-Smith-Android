8c51364c2738de012ceb6bbcde9c5431286e2a4f
Dutra-David <david.kuerten@gmail.com>
2025-12-19 13:14:25 -0300

    Create Hilt AppModule for dependency injection configuration

---
+++ app/src/main/java/com/dutra/agente/di/AppModule.kt
@@ -0,0 +1,40 @@
+package com.dutra.agente.di
+
+import com.dutra.agente.network.RetrofitClient
+import com.dutra.agente.network.ApiService
+import com.dutra.agente.data.repository.MessageRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+/**
+ * AppModule - Hilt dependency injection module
+ * 
+ * Provides singleton instances of:
+ * - ApiService for network calls
+ * - MessageRepository for data access
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+  
+  /**
+   * Provide ApiService instance (singleton)
+   */
+  @Provides
+  @Singleton
+  fun provideApiService(): ApiService {
+    return RetrofitClient.apiService
+  }
+  
+  /**
+   * Provide MessageRepository instance (singleton)
+   */
+  @Provides
+  @Singleton
+  fun provideMessageRepository(): MessageRepository {
+    return MessageRepository()
+  }
+}
