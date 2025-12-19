dd94ebdeab22e136c9c33351b2cb2967fb6b877a
Dutra-David <david.kuerten@gmail.com>
2025-12-19 13:12:09 -0300

    Configure Gradle with all required dependencies (Compose, Retrofit, Hilt, Coroutines)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,23 @@
 // Top-level build file for Agente-Smith-Android
-
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("com.android.library") version "8.1.0" apply false
-    kotlin("android") version "1.9.10" apply false
-    kotlin("kapt") version "1.9.10" apply false
-    id("com.google.dagger.hilt.android") version "2.46" apply false
+  id("com.android.application") version "8.1.0" apply false
+  id("com.android.library") version "8.1.0" apply false
+  kotlin("android") version "1.9.10" apply false
+  kotlin("kapt") version "1.9.10" apply false
+  id("com.google.dagger.hilt.android") version "2.46" apply false
+  id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10" apply false
 }
 
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+  delete(rootProject.buildDir)
 }
+
+// Dependency versions
+extval composeVersion = "1.5.0"
+extval material3Version = "1.1.1"
+extval hiltVersion = "2.46"
+extval retrofitVersion = "2.9.0"
+extval okhttpVersion = "4.11.0"
+extval kotlinxSerializationVersion = "1.6.0"
+extval roomVersion = "2.5.2"
+extval coroutinesVersion = "1.7.1"
